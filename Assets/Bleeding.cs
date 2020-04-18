bdafc418bd6c2482a5db1d7ccb135ba3bd242ec9
vizitas <vizitas7@gmail.com>
2020-04-18 22:03:27 +0300

    bleeding

--- Assets/Bleeding.cs
+++ Assets/Bleeding.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Bleeding : MonoBehaviour
 {
-    public GameObject Particles;
+    public ParticleSystem Particles;
 
     public float CheckInterval = 10;
     public float Probability = 30;
 
+    public float bleedOutTime = 10f;
+    public float healTime = 3f;
+
     private bool bleeding = false;
     private float bleedingStart;
+
+    public int Medkits = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,23 +28,39 @@ public class Bleeding : MonoBehaviour
         if(!bleeding && Random.Range(0, 100) <= Probability)
         {
             bleeding = true;
-            Particles.SetActive(true);
+            var emission = Particles.emission;
+            emission.enabled = true;
             bleedingStart = Time.time;
         }
     }
-
+    bool healing = false;
+    float healingStart;
     private void FixedUpdate()
     {
         if (bleeding)
         {
-            if (Time.time - bleedingStart > 10)
+            if (Time.time - bleedingStart > bleedOutTime)
             {
                 GameStateSingleton.Instance.Death();
             }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                healingStart = Time.time;
+            }
             if (Input.GetKey(KeyCode.F))
             {
+                healing = true;
+            }
+            else
+            {
+                healing = false;
+            }
+
+            if (healing && Time.time - healingStart > healTime)
+            {
                 bleeding = false;
-                Particles.SetActive(false);
+                var emission = Particles.emission;
+                emission.enabled = false;
             }
         }
     }
