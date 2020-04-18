021acb62f65209a2f1a9ab75ef0b3034ab9cc997
vizitas <vizitas7@gmail.com>
2020-04-18 12:03:45 +0300

    Rotation

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform player;
+    private Transform CameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraTransform.position = new Vector3(player.position.x, CameraTransform.position.y, player.position.z);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,32 +7,56 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 1;
 
     CharacterController characterController;
+    Transform playerTransform;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        playerTransform = GetComponent<Transform>();
     }
 
     void FixedUpdate()
     {
-
+        float rotation = 0;
+        bool forwardBackward = false;
+        bool moved = false;
         if (Input.GetKey(KeyCode.W))
         {
             characterController.Move(new Vector3(speed, 0,0));
+            rotation = 0;
+            moved = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             characterController.Move(new Vector3(-1*speed, 0,0));
+            rotation = -180;
+            moved = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             characterController.Move(new Vector3(0, 0, speed));
+            rotation =  -90;
+            moved = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             characterController.Move(new Vector3(0, 0, -1 * speed));
+            rotation =  90;
+            moved = true;
         }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+            rotation = -45;
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+            rotation = 45;
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+            rotation = -135;
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+            rotation = 135;
+        if (moved)
+        playerTransform.rotation = Quaternion.Euler( new Vector3(playerTransform.rotation.eulerAngles.x, rotation, playerTransform.rotation.eulerAngles.z));
+
+
     }
 }
