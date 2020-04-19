bb358032bb758d9ca505cb4e65915de9d486087e
vizitas <vizitas7@gmail.com>
2020-04-19 15:16:45 +0300

    Bullet fix and bullet lighting.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float MinBulletSpeed = 30;
+    public float currentSpeed;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentSpeed = rigidbody.velocity.magnitude;
+        if ( rigidbody.velocity.magnitude < MinBulletSpeed)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
