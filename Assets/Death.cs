175c203524d4e9fba45ad4bb5add92be71ad64d5
vizitas <vizitas7@gmail.com>
2020-04-19 13:04:13 +0300

    Death animation rigging.

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     public PlayerMovement Movement;
+    public Animator playerAnimator;
     // Start is called before the first frame 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +17,7 @@ public class Death : MonoBehaviour
 
     public void Die()
     {
+        playerAnimator.SetBool("Dead", true);
         Movement.enabled = false;
         Invoke("DelayedDeath", 3);
     }
