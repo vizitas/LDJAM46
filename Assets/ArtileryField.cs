acde49dfb835537328ab4d668e1015db09232a5c
vizitas <vizitas7@gmail.com>
2020-04-19 12:29:35 +0300

    Artilery

---
+++ Assets/ArtileryField.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtileryField : MonoBehaviour
+{
+    public GameObject ArtileryHit;
+    public float Frequency = 1f;
+
+    private Collider field;
+    // Update is called once per frame
+    void Start()
+    {
+        field = GetComponent<Collider>();
+        InvokeRepeating("Hit", 0, Frequency);
+
+    }
+    void Hit()
+    {
+        Vector3 location = new Vector3(
+            transform.position.x +  Random.Range(field.bounds.extents.x * -1, field.bounds.extents.x),
+            transform.position.y,
+            transform.position.z +  Random.Range(field.bounds.extents.z * -1, field.bounds.extents.z));
+        var hit = Instantiate(ArtileryHit);
+        hit.transform.position = location;
+    }
+}
