3aaf8bcd8a76704af49dfba3bfba930193c9f26f
vizitas <vizitas7@gmail.com>
2020-04-19 17:53:23 +0300

    Artilery effects and sound.

--- Assets/Artilery.cs
+++ Assets/Artilery.cs
@@ -6,12 +6,30 @@ public class Artilery : MonoBehaviour
 {
     void Start()
     {
-        Invoke("disable", 0.3f);
+
+        Invoke("explosion", 0.6f);
+        Invoke("disable",0.9f);
         Destroy(gameObject, 5);
     }
     private void disable()
     {
         GetComponent<Collider>().enabled = false;
         GetComponent<Light>().enabled = false;
+    }
+
+    private void explosion()
+    {
+        GetComponent<Collider>().enabled = true;
+        GetComponent<Light>().enabled = true;
+        GetComponent<ParticleSystem>().Play();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var deathController = other.GetComponent<Death>();
+            deathController.Die();
+        }
     }
 }
--- Assets/ArtileryField.cs
+++ Assets/ArtileryField.cs
@@ -6,7 +6,7 @@ public class ArtileryField : MonoBehaviour
 {
     public GameObject ArtileryHit;
     public float Frequency = 1f;
-
+    public float probabilty = 50f;
     private Collider field;
     // Update is called once per frame
     void Start()
@@ -17,11 +17,14 @@ public class ArtileryField : MonoBehaviour
     }
     void Hit()
     {
-        Vector3 location = new Vector3(
-            transform.position.x +  Random.Range(field.bounds.extents.x * -1, field.bounds.extents.x),
-            transform.position.y,
-            transform.position.z +  Random.Range(field.bounds.extents.z * -1, field.bounds.extents.z));
-        var hit = Instantiate(ArtileryHit);
-        hit.transform.position = location;
+        if (Random.Range(0, 100) <= probabilty)
+        {
+            Vector3 location = new Vector3(
+                transform.position.x + Random.Range(field.bounds.extents.x * -1, field.bounds.extents.x),
+                transform.position.y,
+                transform.position.z + Random.Range(field.bounds.extents.z * -1, field.bounds.extents.z));
+            var hit = Instantiate(ArtileryHit);
+            hit.transform.position = location;
+        }
     }
 }
